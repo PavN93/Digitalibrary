d28f6975d8ba9f7de46efc7c408eda9911ab42f9
Pawel <paweln993@gmail.com>
2021-03-07 23:50:09 +0000

    updated server file

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
 app.use('/api', require('./routes/api-routes'));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 
